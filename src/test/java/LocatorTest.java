5333abb8b3d3248c8dd3b8203adab8fbfc8f7887
Anton-khm <anton.khmialeuski@gmail.com>
2025-05-05 19:25:08 +0300

    Initial commit

---
+++ src/test/java/LocatorTest.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.time.Duration;
+import java.util.List;
+
+public class LocatorTest extends BaseTest {
+
+    @Test
+    public void checkLocator() {
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name"));
+        driver.findElement(By.name("user-name"));
+        driver.findElement(By.className("submit-button"));
+        driver.findElement(By.tagName("form"));
+    }
+
+}
